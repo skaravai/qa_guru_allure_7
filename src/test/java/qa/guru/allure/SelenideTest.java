2f3c1041414194877bf45149fb721e2757b940ed
skaravai <brizbee0@gmail.com>
2022-04-25 18:02:10 +0300

    Init commit

---
+++ src/test/java/qa/guru/allure/SelenideTest.java
@@ -0,0 +1,27 @@
+package qa.guru.allure;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+import static org.openqa.selenium.By.partialLinkText;
+
+public class SelenideTest {
+
+    @Test
+    public void testGithubIssue() {
+        open("https://github.com ");
+
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys("eroshenkoam/allure-example");
+        $(".header-search-input").submit();
+
+        $(linkText("eroshenkoam/allure-example")).click();
+        $(partialLinkText("Issues")).click();
+        $(withText("#76")).click();
+    }
+
+}
